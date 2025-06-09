431a060426e1a71fb161fb33148145c9b9febc1d
isilacar <isilacar@hotmail.com.tr>
2025-06-09 15:39:06 +0300

    initial commit

---
+++ src/main/java/com/openpayd/currency_management/CurrencyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.openpayd.currency_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyManagementApplication.class, args);
+	}
+
+}
